174a220519974245be2ff3c76ed3f96df1019c73
panganibanmarkjade-spec <panganibanmarkjade@gmail.com>
2025-09-06 20:10:11 +0800

    Add files via upload

---
+++ Activity 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string Fullname = "Mark Jade Panganiban";
+    string CourseYrSec = "BSIT-2I";
+    string Birthday = "April 22, 2006";
+    string Motto = "Keep learning, keep growing";
+    
+    
+    cout<<"Hi I'm "<< Fullname<< " of "<< CourseYrSec<< ". "<< "Welcome to Data Structures and Algorithm!"<<endl;
+    cout<<"My Birthday is on "<< Birthday<< " and my motivation in life is "<< Motto<< "."<<endl;
+
+    return 0;
+}
